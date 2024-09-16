0e1619439920e9f93ef31f93e5a6f00c6dd4bf21
gabwowce <134537965+gabwowce@users.noreply.github.com>
2024-09-16 09:25:03 +0300

    .

---
+++ Controllers/IncomingBooksController.cs
@@ -0,0 +1,58 @@
+﻿using LibraryManagement.Enums;
+using LibraryManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagement.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IncomingBooksController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<IncomingBook>> GetIncomingBooks()
+        {
+            var IncomingBookList = new List<IncomingBook>
+            {
+                 new IncomingBook
+                {
+                    Title = "Shatter Me",
+                    Author = "Tahereh Mafi",
+                    ReleaseYear = 2018,
+                    Price = "12,89 €",
+                    Amount=5,
+                    Status = "Delivered"
+                },
+                  new IncomingBook
+                {
+                     Title = "Can't Hurt Me",
+                    Author = "David Goggins",
+                    ReleaseYear = 2024,
+                    Price = "52,89 €",
+                    Amount=4,
+                    Status = "Delivered"
+                },
+                   new IncomingBook
+                {
+                    Title = "Twisted Hate",
+                    Author = "Ana Huang",
+                    ReleaseYear = 2022,
+                    Price = "13,99 €",
+                    Amount=4,
+                    Status = "Pending"
+                },
+                   new IncomingBook
+                {
+                     Title = "The 48 Laws Of Power",
+                    Author = "28,59 €",
+                    ReleaseYear = 2000,
+                    Price = "28,59 €",
+                    Amount=3,
+                    Status = "Pending"
+                },
+            };
+
+            return Ok(IncomingBookList);
+        }
+    }
+}
+
---
+++ Models/IncomingBook.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement.Models
+{
+    public class IncomingBook
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Price { get; set; }
+        public int Amount { get; set; }
+        public string Status { get; set; }
+
+    }
+}
